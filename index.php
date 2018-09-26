dca3439bcbfde50c1afd569e396cc5c78d2670ab
ypykyns <ypykyns@gmail.com>
2018-09-26 13:58:29 -0300

    inserir index na pasta

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Curso Php Hcode</title>
+</head>
+<body>
+
+	<?php 
+        #echo "Hello World!!";
+
+       $hello = "Hello World?!";
+
+       echo $hello;
+     ?>
+
+</body>
+</html>
