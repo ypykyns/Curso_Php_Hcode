944796c523b6fec18139a7d55e9f02a78622664c
ypykyns <ypykyns@gmail.com>
2018-09-28 15:27:36 -0300

    operadores

---
+++ operadores/atribuicao.php
@@ -0,0 +1,28 @@
+<?php 
+
+#operador de atribuição   = 
+$nome = "Jacinto";
+
+#operador de concatenação   . 
+echo $nome . " Sobrenome"."<br>";
+
+$nome2 = "Josimar";
+$nome2 .= " "."Menezes";
+echo $nome2;
+
+
+$valorTotal=0;
+
+$contador=0;
+
+while ($contador<=25;){
+
+		$valorTotal = $valorTotal + 10;
+		$echo $valorTotal."<br>";
+		$contador++;
+	} 	
+
+
+
+ ?>
+
