effdf66c0b4f6e482ce916d55646144d9d839a7b
ypykyns <ypykyns@gmail.com>
2018-09-28 17:06:44 -0300

    operadoresAritmeticos

--- operadores/atribuicao.php
+++ operadores/atribuicao.php
@@ -8,20 +8,16 @@ echo $nome . " Sobrenome"."<br>";
 
 $nome2 = "Josimar";
 $nome2 .= " "."Menezes";
-echo $nome2;
-
+echo $nome2."<br>";
 
 $valorTotal=0;
-
 $contador=0;
 
-while ($contador<=25;){
-
-		$valorTotal = $valorTotal + 10;
-		$echo $valorTotal."<br>";
+while ($contador < 25){
+		$valorTotal += 10;
+		echo $valorTotal."<br>";
 		$contador++;
 	} 	
-
 
 
  ?>
