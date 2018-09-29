e2b71c2a7166da73e3e582a1fc403c8e8b92de48
ypykyns <lesdsousa@gmail.com>
2018-09-29 01:09:43 -0300

    operadores aritmeticos no mac

--- operadores/operadoresAritmericos.php
+++ operadores/operadoresAritmericos.php
@@ -7,7 +7,40 @@ echo $a+$b;
 
 echo "<br>";
 
-echo $a-$b;
+echo $a-$b."<br>";
+
+echo "<br>";
+echo "<br>";
+
+
+
+
+function reajuste(){
+	$i = 1;
+    $salario = 1100.00;
+
+	while ($i <= 12){
+    
+    $salario *= 1.1;
+    $i++;
+
+    echo "R$ ".number_format($salario,2, ',', '.')."<br>";
+   }
+
+    echo "<br>";
+	echo "<br>";
+
+    echo "Salário Final: "."R$ ".number_format($salario,2, ',', '.')."<br>";
+   
+   //echo "Salário Final: ".$salario;
+};
+
+reajuste();
+
+#echo "Salário Final: ".$salario;
+
+// formatar casas decimais 
+// print $number."->".number_format($number, 2, '.', ',')."<br>";
 
 
   ?>
